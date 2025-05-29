023383bd219bce60b974f14e9d3854819669ef5a
Abdul N.P. Azeez <Abdul.Azeez@nestgroup.net>
2025-05-29 15:07:51 +0530

    add dashboard

---
+++ src/app/components/home/data/dashboard.model.ts
@@ -0,0 +1,11 @@
+
+
+export interface DashboardData {
+  recentSales: any[];
+    recentPurchases: any[];
+    mostSoldItems: any[];
+    totalSales: number;
+    totalRevenue: number;
+    totalPurchases: number;
+    totalCost: number;
+}
